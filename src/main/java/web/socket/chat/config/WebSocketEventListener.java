a7e8d07c4c4b477fa41f1ad4dc5bddde7a48953b
Sokha Rithy <124172865+rithysokha@users.noreply.github.com>
2024-10-30 07:53:38 +0700

    .

--- src/main/java/web/socket/chat/config/WebSocketEventListener.java
+++ src/main/java/web/socket/chat/config/WebSocketEventListener.java
@@ -1,7 +1,6 @@
 package web.socket.chat.config;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
@@ -10,8 +9,9 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 import web.socket.chat.chat.ChatMessage;
 import web.socket.chat.chat.MessageType;
 
+import java.util.Objects;
+
 @Component
-@Slf4j
 @RequiredArgsConstructor
 public class WebSocketEventListener {
 
@@ -20,9 +20,8 @@ public class WebSocketEventListener {
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        String username = (String) Objects.requireNonNull(headerAccessor.getSessionAttributes()).get("username");
         if (username != null) {
-            log.info("user disconnected: {}", username);
             var chatMessage = ChatMessage.builder()
                     .type(MessageType.LEAVE)
                     .sender(username)
